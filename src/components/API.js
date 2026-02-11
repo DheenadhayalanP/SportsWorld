0be38f149bc27f49070afb76f7d8039b94ee584b
DheenadhayalanP <dheenadd18@gmail.com>
2026-02-11 14:39:10 +0530

    connected frontend to render backend

--- src/components/API.js
+++ src/components/API.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000"
+    // baseURL: "http://localhost:5000"
+    baseURL: "https://sportsworld-backend-ltmm.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
